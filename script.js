2860a07d473e833131e95ccf0ddc4b70f4cbf29b
pablo-computer <berwigpablo@gmail.com>
2022-07-06 10:57:25 -0300

    Calculator ready for two number operations

--- script.js
+++ script.js
@@ -7,13 +7,11 @@ let displayFirst = document.getElementById('displayFirst');
 let displayOperator = document.getElementById('displayOperator');
 let displaySecond = document.getElementById('displaySecond');
 
-console.log(numbers);
-console.log(operators);
-
 clear.addEventListener('click', clearDisplay);
 dlt.addEventListener('click', deleteDisplay);
 numbers.forEach(number => number.addEventListener('click', register));
 operators.forEach(operator => operator.addEventListener('click', operate));
+//operators.forEach(operator => operator.addEventListener('click', display));
 
 function register(e){
     if(displayOperator.textContent){
@@ -23,7 +21,7 @@ function register(e){
     } 
 }
 
-function clearDisplay(){
+function clearDisplay(e){
     if(!displayFirst.textContent && !displaySecond.textContent){
         smallDisplay.textContent = '';
     }
@@ -53,15 +51,78 @@ function deleteDisplay(){
     }   
 }
 
-function operate(e){
+function display(e){
 
     if(displayOperator.textContent){
-        smallDisplay.textContent = `${displayFirst.textContent} ${displayOperator.textContent} ${displaySecond.textContent}`
-        clearDisplay();
-
+        smallDisplay.textContent = `${displayFirst.textContent} ${displayOperator.textContent} ${displaySecond.textContent}`;
     } else{
         displayOperator.textContent = e.target.textContent; 
     }
 }
 
+function operate(e){
 
+    if(!displayOperator.textContent && e.target.textContent !== '=' && displayFirst.textContent){
+
+        displayOperator.textContent = e.target.textContent;
+
+    } else if(displayOperator.textContent === '+'){
+    
+        smallDisplay.textContent = `${displayFirst.textContent} ${displayOperator.textContent} ${displaySecond.textContent}`;
+        displayFirst.textContent = Number(displayFirst.textContent) + Number(displaySecond.textContent);
+        displayOperator.textContent = '';
+        displaySecond.textContent = '';
+
+    } else if(displayOperator.textContent === '-'){
+
+        smallDisplay.textContent = `${displayFirst.textContent} ${displayOperator.textContent} ${displaySecond.textContent}`;
+        displayFirst.textContent = Number(displayFirst.textContent) - Number(displaySecond.textContent);
+        displayOperator.textContent = '';
+        displaySecond.textContent = '';
+
+    } else if(displayOperator.textContent === '÷'){
+
+        if(displaySecond.textContent === '0'){
+            displayFirst.textContent = 'nope';
+            displaySecond.textContent = '';
+            displayOperator.textContent = '';
+
+            setTimeout(clearDisplay, 200);
+        }else if(displaySecond.textContent === ''){
+            smallDisplay.textContent = `${displayFirst.textContent} ${displayOperator.textContent}`;
+            displayFirst.textContent = Number(displayFirst.textContent) / 1;
+            displayOperator.textContent = '';
+            displaySecond.textContent = '';
+
+        }else{
+            smallDisplay.textContent = `${displayFirst.textContent} ${displayOperator.textContent} ${displaySecond.textContent}`;
+            displayFirst.textContent = Number(displayFirst.textContent) / Number(displaySecond.textContent);
+            displayOperator.textContent = '';
+            displaySecond.textContent = '';
+        }
+
+    } else if(displayOperator.textContent === '×'){
+
+        smallDisplay.textContent = `${displayFirst.textContent} ${displayOperator.textContent} ${displaySecond.textContent}`;
+        displayFirst.textContent = Number(displayFirst.textContent) * Number(displaySecond.textContent);
+        displayOperator.textContent = '';
+        displaySecond.textContent = '';
+
+    }
+
+    if(e.target.textContent !== '='){
+        displayOperator.textContent = e.target.textContent;
+    }
+
+    if(displayFirst.textContent.split('').includes('.') && !displayOperator.textContent){
+        decimalCount(displayFirst);
+    }
+}
+
+function decimalCount(num){
+    let decCount = num.textContent.split('.');
+
+    if(decCount[1].length > 4){
+        displayFirst.textContent = Number(displayFirst.textContent).toFixed(4);
+    }
+}
